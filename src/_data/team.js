1ba407337585bb112de66b581cc6e4c1be6a248d
Olivier Rohellec <olivier@pc-117.home>
2025-06-06 15:23:06 +0200

    almost finished to implement team page

---
+++ src/_data/team.js
@@ -0,0 +1,97 @@
+module.exports = {
+  management: [
+    {
+      name: "Yves",
+      pictureUrl: "/assets/images/team/yves.jpg",
+      informations: ["Président"]
+    },
+        {
+      name: "Christine",
+      pictureUrl: "/assets/images/team/christine.jpeg",
+      informations: ["Trésorière et responsable administrative"]
+    },
+        {
+      name: "Monique",
+      pictureUrl: "/assets/images/team/monique.jpeg",
+      informations: ["Secrétaire"]
+    },
+        {
+      name: "Janet",
+      pictureUrl: "/assets/images/team/janet.jpg",
+      informations: ["Conseillère - Membre d'honneur"]
+    },
+        {
+      name: "Uta",
+      pictureUrl: "/assets/images/team/uta.jpg",
+      informations: ["Conseillère - Membre d'honneur"]
+    },
+        {
+      name: "Philippe",
+      pictureUrl: "/assets/images/team/philippe.jpeg",
+      informations: ["Conseiller"]
+    },
+        {
+      name: "Patrick",
+      pictureUrl: "/assets/images/team/patrick.jpeg",
+      informations: ["Conseiller"]
+    },
+        {
+      name: "Saoussen",
+      pictureUrl: "/assets/images/team/saoussen.jpeg",
+      informations: ["Conseillère"]
+    },
+  ],
+    teachers: [
+    {
+      name: "Elise",
+      pictureUrl: "/assets/images/team/elise.jpg",
+      informations: ["Anglais - Langue maternelle", "Licence et Master MEEF anglais - CAPES", "Sorbonne Nouvelle"]
+    },
+        {
+      name: "Jacqueline",
+      pictureUrl: "/assets/images/team/jacqueline.jpeg",
+      informations: ["Anglais - Langue maternelle", "BSc (Hons) Physics / Diplôme d'ingénieur", "Manchester University (UK) / UTCompiègne"]
+    },
+        {
+      name: "Louis",
+      pictureUrl: "/assets/images/team/louis.jpeg",
+      informations: ["Anglais - Langue maternelle", "Licence d'anglais / Master ESRA en cours", "Sorbonne Nouvelle"]
+    },
+        {
+      name: "Karin",
+      pictureUrl: "/assets/images/team/karin.jpeg",
+      informations: ["Allemand - Langue maternelle", "Maîtrise des troubles du langage", "P.H. Reutlingen / Universität Tübingen (DE)"]
+    },
+        {
+      name: "Kurdistan",
+      pictureUrl: "/assets/images/team/kurdistan.jpeg",
+      informations: ["Arabe littéraire - Langue maternelle", "Licence en droit"]
+    },
+        {
+      name: "Diane",
+      pictureUrl: "/assets/images/team/diane.jpg",
+      informations: ["Chinois - Langue maternelle", "Master of Science Information Systems", "Texas University - Arlington (USA)"]
+    },
+        {
+      name: "Teresa",
+      pictureUrl: "/assets/images/team/teresa.jpeg",
+      informations: ["Espagnol - Langue maternelle", "Licence de journalisme (relat.publiques)", "Universidad Complutense Madrid (ESP)"]
+    },
+        {
+      name: "Ileana",
+      pictureUrl: "/assets/images/team/ileana.jpeg",
+      informations: ["Italien - Langue maternelle", "Diplôme de formation des enseignants", "Inligua-Como / Inst. Dante Alighieri-Siena (IT)"]
+    },
+    {
+      name: "Recrutement en cours",
+      pictureUrl: "/assets/images/team/femme_anonyme.png",
+      informations: ["Russe - Langue maternelle"]
+    },
+    {
+      name: "Recrutement en cours",
+      pictureUrl: "/assets/images/team/femme_anonyme.png",
+      informations: ["Français (FLE) - Langue maternelle"]
+    }
+    
+    ]
+}
